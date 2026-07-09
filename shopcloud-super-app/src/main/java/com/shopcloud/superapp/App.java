7cf23c44ff2ae91e4301f41c6feef3506f03f7db
Nguyen-Thanh-Danh-0711 <nthanhdanh0711@gmail.com>
2026-07-09 13:33:25 +0000

    Tạo bộ khung javafx

---
+++ shopcloud-super-app/src/main/java/com/shopcloud/superapp/App.java
@@ -0,0 +1,13 @@
+package com.shopcloud.superapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
